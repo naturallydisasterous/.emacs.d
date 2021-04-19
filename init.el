a4ae714317eee5b468d3e566d8859a0af60ff2c2
Aaron-Mann <46825032+Aaron-Mann@users.noreply.github.com>
2021-04-19 08:51:14 -0700

    (custom-set-variables) begone!

--- init.el
+++ init.el
@@ -6,15 +6,6 @@
 (toaster-log "Libraries loaded. Starting core...")
 
 (toaster-initialize)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil doom-themes)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+;; Anything past this line was autofilled by emacs under (custom-set-variables)
+;; ===========================================================================
